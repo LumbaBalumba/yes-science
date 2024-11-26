b00b7a88c35bec8e42c54215b4d183f6e5e42314
LumbaBalumba <vlad.lazar.ik528@yandex.ru>
2024-11-26 17:54:58 +0300

    .,

--- models.py
+++ models.py
@@ -1,17 +1,19 @@
 import numpy as np
-from scipy.optimize import LinearConstraint, minimize
-from scipy.ndimage import gaussian_filter1d
-from scipy.signal import find_peaks
+from scipy.optimize import Bounds, LinearConstraint, minimize
+from itertools import chain, combinations
+from scipy.signal import savgol_filter, find_peaks
+
+EPS = 1e-8
 
 
-class ExpModel:
+class PBFTPK:
     D: float
     F: float
     V_d: float
     k_a: float
     k_el: float
-    tau: list
-    modes: list
+    tau: float
+    alpha: float
 
     @staticmethod
     def _base_model(
@@ -21,38 +23,38 @@ class ExpModel:
         V_d: float,
         k_a: float,
         k_el: float,
-        tau_arr: list[float],
-        modes: list[str],
+        tau: float,
     ) -> np.ndarray:
-        tau_arr += [np.inf]
-        C_cur = 0.0
-        tau_cur = 0.0
-
         X = np.zeros_like(t)
 
         def absorption_model(t: np.ndarray | float) -> np.ndarray | float:
-            return C_cur + F * D * k_a / V_d / (k_a - k_el) * (
-                np.exp(-k_el * t) - np.exp(-k_a * t)
+            return (
+                F
+                * D
+                * k_a
+                / V_d
+                / (k_a - k_el)
+                * (np.exp(-k_el * t) - np.exp(-k_a * t))
             )
 
-        def elimination_model(t: np.ndarray | float) -> np.ndarray | float:
-            return C_cur * np.exp(-k_el * (t - tau_cur))
+        X[t <= tau] = absorption_model(t[t <= tau])
+        C_max = X[t <= tau][-1] if len(X[t <= tau]) > 0 else 1
 
-        for tau, mode in zip(tau_arr, modes):
-            idx = (tau_cur < t) & (t <= tau)
-            match mode:
-                case "up":
-                    X[idx] = absorption_model(t[idx])
-                case "down":
-                    X[idx] = elimination_model(t[idx])
-            tau_cur = tau
-            C_cur = X[idx][-1] if len(X[idx]) > 0 else C_cur
+        def elimination_model(t: np.ndarray | float) -> np.ndarray | float:
+            return C_max * np.exp(-k_el * (t - tau))
+
+        X[t > tau] = elimination_model(t[t > tau])
+
         return X
 
-    def __init__(self, tau: list, modes: list) -> None:
+    def __init__(self, tau: float, alpha: float = 1.0) -> None:
         self.initilized = False
         self.tau = tau
-        self.modes = modes
+        self.alpha = alpha
+
+    def set_params(self, D: float, F: float, V_d: float, k_a: float, k_el: float):
+        self.initilized = True
+        self.D, self.F, self.V_d, self.k_a, self.k_el = D, F, V_d, k_a, k_el
 
     def fit(self, t: np.ndarray, X: np.ndarray) -> None:
         self.initilized = True
@@ -64,9 +66,8 @@ class ExpModel:
         )
 
         def target_function(params):
-            r = (
-                ExpModel._base_model(t, *params, tau_arr=self.tau, modes=self.modes) - X
-            ) ** 2
+            r = (PBFTPK._base_model(t, *params, tau=self.tau) - X) ** 2
+            r[t > self.tau] *= self.alpha
             return np.mean(r)
 
         res = minimize(
@@ -75,41 +76,132 @@ class ExpModel:
 
         self.D, self.F, self.V_d, self.k_a, self.k_el = res.x
 
-    def sample(self, t=np.linspace(0, 100, 1000)):
-        return ExpModel._base_model(
-            t,
-            D=self.D,
-            F=self.F,
-            V_d=self.V_d,
-            k_a=self.k_a,
-            k_el=self.k_el,
-            tau_arr=self.tau,
-            modes=self.modes,
+    def sample(self, t: np.ndarray) -> np.ndarray:
+        return PBFTPK._base_model(
+            t, self.D, self.F, self.V_d, self.k_a, self.k_el, self.tau
         )
 
 
-class TauModel:
+class MultiPBFTPK:
+    N_PARAMS = 5
+
+    n_models: int
     tau: list
+    alpha: list
 
-    def __init__(self) -> None:
-        pass
-
-    def fit(self, t: np.ndarray, X: np.ndarray):
-        X_smooth = gaussian_filter1d(X, sigma=2)
-
-        peaks, _ = find_peaks(X_smooth)
-        valleys, _ = find_peaks(-X_smooth)
-        print(peaks, valleys)
-
-        self.tau = list(t[sorted(peaks + valleys)])
-
-
-class PBFTPK:
-    exp_model: ExpModel
-    tau_model: TauModel
-
-    def __init__(self) -> None:
-        pass
+    def __init__(self, tau: list = [0.05], alpha: list = [1.0]) -> None:
+        self.n_models = len(tau)
+        self.params = np.random.uniform(
+            low=1, high=2, size=(self.n_models, self.N_PARAMS)
+        )
+        self.tau = tau
+        self.alpha = alpha
 
     def fit(self, t: np.ndarray, X: np.ndarray) -> None:
-        self.tau_model.fit(t, X)
+
+        lb = [EPS, 0, EPS, EPS, EPS] * self.n_models
+        ub = [np.inf, 1, np.inf, np.inf, np.inf] * self.n_models
+
+        bounds = Bounds(lb, ub)
+
+        lb = []
+        ub = []
+        matr = []
+
+        for i in range(self.n_models):
+            matr.append(
+                [0] * self.N_PARAMS * i
+                + [0, 0, 0, 1, -1]
+                + [0] * self.N_PARAMS * (self.n_models - i - 1)
+            )
+            lb.append(-np.inf)
+            ub.append(-EPS)
+
+            if i > 0:
+                row = [0] * self.N_PARAMS * self.n_models
+                row[self.N_PARAMS - 1 + self.N_PARAMS * i] = 1
+                row[self.N_PARAMS - 1 + self.N_PARAMS * (i - 1)] = -1
+                matr.append(row)
+                lb.append(EPS)
+                ub.append(np.inf)
+
+        cons = LinearConstraint(matr, lb, ub)
+
+        def target_function(params):
+            params = params.reshape((self.n_models, self.N_PARAMS))
+            X_predict = np.zeros_like(X)
+            for i, param_row in enumerate(params):
+                D, F, V_d, k_a, k_el = param_row
+                tau = self.tau[i]
+                model = PBFTPK(tau, 1.0)
+                model.set_params(D, F, V_d, k_a, k_el)
+                X_predict += model.sample(t)
+            r = (X - X_predict) ** 2
+            for tau, alpha in zip(self.tau, self.alpha):
+                r[t > tau] += r[t > tau] * (alpha - 1.0)
+            return np.mean(r)
+
+        res = minimize(
+            target_function,
+            method="COBYLA",
+            x0=self.params.reshape((-1)),
+            constraints=cons,
+            bounds=bounds,
+            options={"maxiter": 1000000},
+        )
+        if not res.success:
+            print(res.message)
+
+        self.params = res.x.reshape((self.n_models, self.N_PARAMS))
+
+    def set_params(self, params):
+        self.params = np.array(params)
+        self.n_models = len(params)
+
+    def sample(self, t: np.ndarray) -> np.ndarray:
+        X_predict = np.zeros_like(t)
+        for i, param_row in enumerate(self.params):
+            tau = self.tau[i]
+            D, F, V_d, k_a, k_el = param_row
+            model = PBFTPK(tau, 1.0)
+            model.set_params(D, F, V_d, k_a, k_el)
+            X_predict += model.sample(t)
+        return X_predict
+
+
+class MainModel:
+    model: MultiPBFTPK
+
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def find_peaks(X: np.ndarray):
+        return find_peaks(savgol_filter(X, 7, 3))[0]
+
+    def fit(self, t: np.ndarray, X: np.ndarray) -> None:
+        def powerset(iterable):
+            s = list(iterable)
+            return chain.from_iterable(combinations(s, r) for r in range(1, len(s) + 1))
+
+        X_peaks = MainModel.find_peaks(X)
+
+        cur_peaks = []
+        cur_result = np.inf
+
+        for peaks in powerset(X_peaks):
+            model = MultiPBFTPK(
+                n_models=len(peaks), tau=list(peaks), alpha=[1] * len(peaks)
+            )
+            model.fit(t, X)
+
+            r = np.mean((model.sample(t) - X) ** 2)
+            if r < cur_result:
+                cur_result = r
+                cur_peaks = peaks
+        self.model = MultiPBFTPK(
+            n_models=len(cur_peaks), tau=list(cur_peaks), alpha=[1] * len(cur_peaks)
+        )
+
+    def sample(self, t: np.ndarray) -> np.ndarray:
+        return self.model.sample(t)
