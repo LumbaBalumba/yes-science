3be05a1aa097e5117146e1f730ba314c961cbaf9
LumbaBalumba <vlad.lazar.ik528@yandex.ru>
2025-01-24 23:41:31 +0300

    .

--- models.py
+++ models.py
@@ -1,9 +1,33 @@
 import numpy as np
 from scipy.optimize import Bounds, LinearConstraint, minimize
+from scipy.signal import find_peaks
 from sklearn.preprocessing import MinMaxScaler as Scaler
+from sklearn.metrics import mean_squared_error
 
 
-class PBFTPK:
+def _find_peaks(t: np.ndarray, X: np.ndarray, t_max: float) -> tuple[float, float]:
+    peaks, _ = find_peaks(X[t < t_max])
+    if len(peaks) > 0:
+        tau_2 = float(t[peaks[-1]])
+    else:
+        tau_2 = t[-1]
+    volleys, _ = find_peaks(-X[t < tau_2])
+    if len(volleys) > 0:
+        tau_1 = float(t[volleys[-1]])
+    else:
+        tau_1 = t[0]
+    return tau_1, tau_2
+
+
+class BaseModel:
+    def fit(self, t: np.ndarray, X: np.ndarray) -> None:
+        pass
+
+    def sample(self, t: np.ndarray) -> np.ndarray:
+        return np.zeros_like(t)
+
+
+class PBFTPK(BaseModel):
     D: float
     F: float
     V_d: float
@@ -11,6 +35,7 @@ class PBFTPK:
     k_el: float
     tau_1: float
     tau_2: float
+    tau_3: float
     alpha: float
     scaler: Scaler
 
@@ -28,13 +53,14 @@ class PBFTPK:
         X = np.zeros_like(t)
 
         def absorption_model(t: np.ndarray | float) -> np.ndarray | float:
+            t = np.array(t) - tau_1
             return (
                 F
                 * D
                 * k_a
                 / V_d
                 / (k_a - k_el)
-                * (np.exp(-k_el * t) - np.exp(-k_a * t))
+                * (np.exp(-k_el * (t)) - np.exp(-k_a * (t)))
             )
 
         idx_a = (tau_1 < t) & (t <= tau_2)
@@ -42,6 +68,7 @@ class PBFTPK:
         C_max = X[idx_a][-1] if len(X[idx_a]) > 0 else 1
 
         def elimination_model(t: np.ndarray | float) -> np.ndarray | float:
+            t = np.array(t) - tau_1
             return C_max * np.exp(-k_el * (t - tau_2))
 
         idx_el = t > tau_2
@@ -49,9 +76,10 @@ class PBFTPK:
 
         return X
 
-    def __init__(self, alpha: float = 1.0) -> None:
+    def __init__(self, alpha: float = 1.0, tau_3: float = float("inf")) -> None:
         self.initilized = False
         self.alpha = alpha
+        self.tau_3 = tau_3
 
     def fit(self, t: np.ndarray, X: np.ndarray) -> None:
         self.initilized = True
@@ -63,9 +91,9 @@ class PBFTPK:
         t = data[:, 0]
         X = data[:, 1]
 
-        self.tau_1 = t[np.argmin(X)]
+        self.tau_1, self.tau_2 = _find_peaks(t, X, self.tau_3)
+
         X[t < self.tau_1] = 0.0
-        self.tau_2 = t[np.argmax(X)]
 
         params_initial = [1.3, 0.5, 1, 1, 2]
 
@@ -114,21 +142,26 @@ class PBFTPK:
         return X
 
 
-class EnsembledPBFTPK:
+class EnsembledPBFTPK(BaseModel):
     n_models: int
     models: list[PBFTPK]
 
-    def __init__(self, n_models: int, alpha: float | list = 1.0):
+    def __init__(
+        self, n_models: int = 1, alpha: float | list[float] | np.ndarray = 1.0
+    ):
         self.n_models = n_models
-        self.models = []
         self.alpha = np.ones(n_models) * alpha
+        self.models = []
 
     def fit(self, t: np.ndarray, X: np.ndarray) -> None:
         r = X.copy()
+        tau_3 = float("inf")
         for i in range(self.n_models):
-            self.models.append(PBFTPK(alpha=self.alpha[i]))
-            self.models[-1].fit(t, r)
-            r -= self.models[-1].sample(t)
+            model = PBFTPK(alpha=self.alpha[i], tau_3=tau_3)
+            model.fit(t, r)
+            self.models.append(model)
+            r -= self.models[i].sample(t)
+            tau_3 = model.tau_2
 
     def sample(self, t: np.ndarray) -> np.ndarray:
         X = np.zeros_like(t)
@@ -137,31 +170,27 @@ class EnsembledPBFTPK:
         return X
 
 
-class AdaptiveEnsembledPBFTPK:
-    n_models: int
-    model: EnsembledPBFTPK | None
+class EffectiveEnsembledPBFTPK(BaseModel):
+    n_opt: int
+    n_max: int
+    alpha: float
+    model: EnsembledPBFTPK
 
-    def __init__(self, n_models: int):
-        assert n_models > 0
-        self.n_models = n_models
-        self.model = None
+    def __init__(self, n_max: int, alpha: float) -> None:
+        self.n_max = n_max
+        self.n_opt = 0
+        self.alpha = alpha
 
     def fit(self, t: np.ndarray, X: np.ndarray) -> None:
-        assert t.shape == X.shape
-
-        bounds = Bounds(lb=[0.2] * self.n_models, ub=[10.0] * self.n_models)
-
-        def target_function(alpha):
-            model = EnsembledPBFTPK(self.n_models, alpha)
-            model.fit(t, X)
-            return np.mean((X - model.sample(t)) ** 2)
-
-        alpha_initial = np.ones(self.n_models)
-
-        res = minimize(target_function, bounds=bounds, x0=alpha_initial, method="SLSQP")
-
-        self.model = EnsembledPBFTPK(self.n_models, res.x)
+        models = [
+            EnsembledPBFTPK(n_models=i, alpha=self.alpha)
+            for i in range(1, self.n_max + 1)
+        ]
+        samples = [model.sample(t) for model in models]
+        errors = [mean_squared_error(sample, X) for sample in samples]
+        self.n_opt = int(np.argmin(errors)) + 1
+        self.model = EnsembledPBFTPK(n_models=self.n_opt, alpha=self.alpha)
+        self.model.fit(t, X)
 
     def sample(self, t: np.ndarray) -> np.ndarray:
-        assert self.model is not None
         return self.model.sample(t)
